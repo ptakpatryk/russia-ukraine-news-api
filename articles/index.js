c50126502a25c33faafbd2929a590a713dd924f1
Patryk Ptak <ptaszqq@gmail.com>
2022-02-26 10:54:39 +0000

    first commit

---
+++ articles/index.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getArticles = async () => {
+  const articles = [];
+
+  const response = await axios.get(
+    'https://www.bbc.co.uk/news/live/world-europe-60517447'
+  );
+  const html = response.data;
+
+  const $ = cheerio.load(html);
+  $('li.lx-stream__post-container', html).each(function () {
+    const time = $('.qa-post-auto-meta', this).text();
+    const title = $('h3 .lx-stream-post__header-text', this).text();
+    if (title.includes("If you're just joining us")) return;
+    const breaking = $('article.lx-stream-post--breaking', this).length;
+    let body = [];
+
+    $('.lx-stream-post-body p', this).each(function () {
+      body.push($(this).text());
+    });
+
+    let images = [];
+
+    $('.lx-media-asset__image', this).each(function () {
+      let img = $('img', this);
+      img = img[0].attribs;
+      const imgWidths = JSON.parse(img['data-widths']);
+      img = img['data-src'].replace('{width}', imgWidths[imgWidths.length - 1]);
+
+      images.push(img);
+    });
+
+    articles.push({
+      time,
+      title,
+      hasImages: Boolean(images.length),
+      isBreaking: Boolean(breaking),
+      body,
+      images,
+    });
+  });
+
+  return articles;
+};
+
+module.exports = getArticles;
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const getArticles = require('./articles');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json('Welcome to my Climate Change News API');
+});
+
+app.get('/news', async (req, res) => {
+  try {
+    const articles = await getArticles();
+    res.json(articles);
+  } catch (e) {
+    throwError(res);
+  }
+});
+
+app.get('/news-latest', async (req, res) => {
+  try {
+    const articles = await getArticles();
+
+    res.json(articles[0]);
+  } catch (e) {
+    throwError(res);
+  }
+});
+
+app.get('/summary', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://www.bbc.co.uk/news/live/world-europe-60517447'
+    );
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const summary = [];
+
+    $('ol.lx-c-summary-points li').each(function () {
+      summary.push($(this).text());
+    });
+
+    res.json(summary);
+  } catch (e) {
+    throwError(res);
+  }
+});
+
+function throwError(res) {
+  console.error("Couldn't load the BBC website.");
+  res.status(400);
+  res.json("Couldn't load the BBC website.");
+}
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
