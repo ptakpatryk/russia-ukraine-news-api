b260461865290fa8cebb830672a17287d434c3c7
Patryk Ptak <ptaszqq@gmail.com>
2022-03-19 07:46:52 +0000

    new url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const getArticles = require('./articles');
 
 const PORT = process.env.PORT || 3000;
 
-global._url = 'https://www.bbc.co.uk/news/live/world-europe-60746557';
+global._url = 'https://www.bbc.co.uk/news/live/world-europe-60802572';
 
 const app = express();
 
